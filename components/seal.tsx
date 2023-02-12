0be5378ff80c265e4372563ba2933bae029b1358
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-02-12 12:44:06 -0500

    fix lint

--- components/seal.tsx
+++ components/seal.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
     
-export default function Seal({ width = 75, height = 75, ...props }: HTMLImageElement) {
+export default function Seal() {
     return (
         <>
             <p className="cachet">
-                <Image src="/seal.png" width={width} height={height} alt="All For Me, One for Me" className="w-2" />
+                <Image src="/seal.png" width={75} height={75} alt="All For Me, One for Me" className="w-2" />
             </p>
             <div id="signature">Kryn&apos;s Greatest Hero<br />Sennet</div>
         </>
